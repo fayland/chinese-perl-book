b31d9022e81a694a5e07752e5edcf6d69575be49
fayland <fayland@gmail.com>
2009-07-29 14:32:45 +0800

    move into Result::

--- eg/DBIx-Class/lib/TheSchwartz/Schema.pm
+++ eg/DBIx-Class/lib/TheSchwartz/Schema.pm
@@ -5,12 +5,6 @@ use warnings;
 
 use base 'DBIx::Class::Schema';
 
-__PACKAGE__->load_classes;
+__PACKAGE__->load_namespaces;
 
-
-# Created by DBIx::Class::Schema::Loader v0.04006 @ 2009-07-28 22:58:22
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:uezOBdwMXGXtUvxp5ohQKg
-
-
-# You can replace this text with custom content, and it will be preserved on regeneration
 1;
