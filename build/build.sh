8acc43dbe18d38e5c601ebe00fcd2043de60c006
fayland <fayland@gmail.com>
2009-08-06 17:07:30 +0800

    mkdir and rm

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-echo "Copying POD/eg to lib";
+echo "Copying POD/eg to lib"
+mkdir lib/Book/Chinese/MasterPerlToday
 cp -fv ../*.pod lib/Book/Chinese/MasterPerlToday/
 cp -fvr ../eg lib/Book/Chinese/MasterPerlToday/
 
 echo "dzil build"
 dzil build
 
-echo "Done";
+echo "Clean Up";
+rm -fvr lib/Book/Chinese/MasterPerlToday
